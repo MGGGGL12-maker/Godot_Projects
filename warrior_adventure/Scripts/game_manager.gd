e5e562a407205e16362a99336e077e8dcd1ead8a
MGGGGL12-maker <jmocarvallho7123@gmail.com>
2025-08-29 15:28:57 -0300

    Finalização do terreno do Jogo

--- warrior_adventure/Scripts/game_manager.gd
+++ warrior_adventure/Scripts/game_manager.gd
@@ -2,7 +2,10 @@ extends Node
 
 var TotalScore: int = 0
 @onready var score_label: Label = $ScoreLabel
+@onready var score_label_2: Label = $ScoreLabel2
 
 func Add_Point(score):
 	TotalScore += score
 	score_label.text = "You collected " + str(TotalScore) + " coins."
+	score_label_2.text = "You collected " + str(TotalScore) + " coins."
+	
