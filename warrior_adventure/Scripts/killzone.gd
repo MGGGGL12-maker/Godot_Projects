6ee09c94a76d2a653415a0fab4d0758e4b74c6d1
MGGGGL12-maker <jmocarvallho7123@gmail.com>
2025-08-29 14:14:38 -0300

    Programando a Killzone

--- warrior_adventure/Scripts/killzone.gd
+++ warrior_adventure/Scripts/killzone.gd
@@ -3,10 +3,12 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You Died!")
+	Engine.time_scale = 0.4
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+	Engine.time_scale = 1
