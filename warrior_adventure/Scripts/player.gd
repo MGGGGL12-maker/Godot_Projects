f23a12d471637becba8f476abb6fbaaff8d70120
MGGGGL12-maker <jmocarvallho7123@gmail.com>
2025-08-29 14:54:14 -0300

    Modificações Extras

--- warrior_adventure/Scripts/player.gd
+++ warrior_adventure/Scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 170.0
 const JUMP_VELOCITY = -350.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	Move()
@@ -13,11 +14,17 @@ func _physics_process(delta: float) -> void:
 func Move():
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("Move_Left", "Move_Right")
 	if direction:
+		if direction < 0:
+			animated_sprite_2d.flip_h = true
+		elif direction > 0:
+			animated_sprite_2d.flip_h = false
 		velocity.x = direction * SPEED
+		animated_sprite_2d.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite_2d.play("Idle")
 
 	move_and_slide()
 
@@ -26,7 +33,8 @@ func Jump(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animated_sprite_2d.play("Jump")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
